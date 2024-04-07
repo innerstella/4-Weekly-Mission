44ae9cdeb83a0e35ad82ced08507d2e1a5269c51
innerstella <suji0509@naver.com>
2024-04-07 14:58:12 +0900

    feat: 액세스 토큰 보유 시 폴더 페이지로 이동

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,9 +1,19 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 import ChangeSignInUp from "@/src/signin-signup/change-signin-up";
 import Layout from "@/src/signin-signup/layout";
 import SigninForm from "@/src/signin-signup/signin-form";
 import SocialLogin from "@/src/signin-signup/social-login";
+import { hasAccessToken } from "@/src/utils/hasAccessToken";
 
 export default function SigninPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    hasAccessToken() && router.push("/folder");
+  }, []);
+
   return (
     <Layout>
       <ChangeSignInUp currPage="signin" />
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,9 +1,18 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 import ChangeSignInUp from "@/src/signin-signup/change-signin-up";
 import Layout from "@/src/signin-signup/layout";
 import SignupForm from "@/src/signin-signup/signup-form";
 import SocialLogin from "@/src/signin-signup/social-login";
+import { hasAccessToken } from "@/src/utils/hasAccessToken";
 
 export default function SignupPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    hasAccessToken() && router.push("/folder");
+  }, []);
   return (
     <Layout>
       <ChangeSignInUp currPage="signup" />
